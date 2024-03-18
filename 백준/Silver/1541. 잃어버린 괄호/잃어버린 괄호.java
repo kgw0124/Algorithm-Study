8bdbcc4d4174a9193a21d11a5f0a16f329598b59
ga0nii <123241025+kgw0124@users.noreply.github.com>
2024-03-18 10:56:32 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 124 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -10,35 +10,26 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String input = br.readLine();
 
-        // (1) input을 1. 두 자리 이상의 수 2. - 3. +을 기준으로 쪼갠다.
-        Pattern pattern = Pattern.compile("\\d+|[-+]");
-        Matcher matcher = pattern.matcher(input);
+        // (1) input을 -를 기준으로 쪼개기 : 각 index에 1. 숫자 2. 숫자+숫자 형식으로 들어간다.
+        String[] minusArr = input.split("-");
 
-        // (2) (1)의 결과값을 +인 경우, 계산을 수행한다. +가 아닌 경우, arr 뒤에 삽입한다.
-        ArrayList<String> arr = new ArrayList<>();
-        while(matcher.find()){
-            String temp = matcher.group();
-            switch (temp){
-                case "+":
-                    int x = Integer.parseInt(arr.get(arr.size()-1));
-                    int y = 0;
-                    if(matcher.find()){
-                        y = Integer.parseInt(matcher.group());
-                    }
-                    arr.set(arr.size()-1, String.valueOf(x+y));
-                    break;
-                default:
-                    arr.add(temp);
-                    break;
+        // (2) 각 index를 순회하면서 +를 포함한 경우, +를 기준으로 쪼갠 후 더하기 연산을 수행한다.
+        for(int i=0; i<minusArr.length; i++){
+            if(minusArr[i].contains("+")){
+                String[] plusArr = minusArr[i].split("\\+");
+                int temp = Integer.parseInt(plusArr[0]);
+                for(int j=1; j<plusArr.length; j++){
+                    temp += Integer.parseInt(plusArr[j]);
+                }
+                minusArr[i] = Integer.toString(temp);
             }
         }
 
-        // (3) arr의 앞부터 차례대로 뺄셈 연산을 수행한다. 이때 arr의 짝수 인덱스는 수, 홀수 인덱스에는 -가 저장되어 있다.
-        int result = Integer.parseInt(arr.get(0));
-        for(int i=2; i<arr.size(); i+=2){
-            result -= Integer.parseInt(arr.get(i));
+        // (3) 뺄셈 수행
+        int result = Integer.parseInt(minusArr[0]);
+        for(int i=1; i<minusArr.length; i++){
+            result -= Integer.parseInt(minusArr[i]);
         }
-
         System.out.println(result);
     }
 }
