6a2a0f8fe37d7ce65f2fb41b3274befa4b51f91b
ga0nii <123241025+kgw0124@users.noreply.github.com>
2024-04-08 13:09:47 +0900

    [Gold IV] Title: 최단경로, Time: 768 ms, Memory: 107772 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -7,40 +7,57 @@ import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main{
-    static int V;
-    static ArrayList<Node>[] graph;
-    static boolean[] checker;
-    static int[] distance;
+    static class Node implements Comparable<Node>{
+        int next;
+        int weight;
+        Node(int next, int weight){
+            this.next = next;
+            this.weight = weight;
+        }
 
-    public static void main(String[] args)throws IOException {
+        @Override
+        public int compareTo(Node n){
+            return weight - n.weight;
+        }
+    }
+
+    static ArrayList<Node>[] graph = null;
+    static int[] distance = null;
+    static boolean[] checker = null;
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        V = Integer.parseInt(st.nextToken());
-        int E = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(br.readLine());
+        int V = Integer.parseInt(st.nextToken()); // 정점 개수
+        int E = Integer.parseInt(st.nextToken()); // 간선 개수 
+        int K = Integer.parseInt(br.readLine()); // 출발 노드
 
-        // graph
+        // graph 채우기
         graph = new ArrayList[V+1];
-        for (int i = 1; i < V+1; i++){
+        for(int i=1; i<V+1; i++){
             graph[i] = new ArrayList<>();
         }
-        for (int i = 0; i < E; i++){
+        for(int i=0; i<E; i++){
             st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
             int w = Integer.parseInt(st.nextToken());
+
             graph[x].add(new Node(y, w));
         }
+
+        distance = new int[V+1];
+        checker = new boolean[V+1];
         
-        // 디엑스트라
+        // 다익스트라 시작
         dij(K);
 
         // 정답 출력
         StringBuilder sb = new StringBuilder();
-        for (int i=1; i<V+1; i++){
-            if (distance[i] == Integer.MAX_VALUE) {
+        for(int i=1; i<V+1; i++){
+            if(distance[i] == Integer.MAX_VALUE){
                 sb.append("INF").append("\n");
-            } else {
+            }else{
                 sb.append(distance[i]).append("\n");
             }
         }
@@ -48,42 +65,27 @@ public class Main{
     }
 
     public static void dij(int start){
-        // distance
-        distance = new int[V+1];
+        // [1] distance 배열을 짱 큰 수로 채운다.
         Arrays.fill(distance, Integer.MAX_VALUE);
-        // checker
-        checker = new boolean[V+1];
 
-        PriorityQueue<Node> queue = new PriorityQueue<>();
-        distance[start] = 0;
-        queue.offer(new Node(start,0));
+        // [2] 기본 세팅
+        PriorityQueue<Node> queue = new PriorityQueue<>(); // 우선순위 큐 생성
+        queue.add(new Node(start, 0)); // 큐에 시작 노드 넣기
+        distance[start] = 0; // 시작 노드 거리 0으로 세팅
 
-        while (!queue.isEmpty()){
+        // [3] 최단 거리 탐색 시작
+        while(!queue.isEmpty()){
             Node temp = queue.poll();
-            checker[temp.next] = true;
-            for(Node node : graph[temp.next]){
-                if (!checker[node.next]){
-                    if (distance[temp.next] + node.weight < distance[node.next]){
-                        distance[node.next] = distance[temp.next] + node.weight;
-                        queue.offer(new Node(node.next, distance[node.next]));
-                    }
+            checker[temp.next] = true; // 방문 처리
+
+            if(temp.weight > distance[temp.next]) continue;
+
+            for(Node n : graph[temp.next]){ // n : temp에서 이동할 다음 노드
+                if(!checker[n.next] && distance[temp.next] + n.weight < distance[n.next]){
+                    distance[n.next] = distance[temp.next] + n.weight;
+                    queue.add(new Node(n.next, distance[n.next]));
                 }
             }
         }
-    }
-}
-
-class Node implements Comparable<Node>{
-    public int next;
-    public int weight;
-
-    @Override
-    public int compareTo(Node n){
-        return this.weight - n.weight;
-    }
-
-    public Node(int next, int weight) {
-        this.next = next;
-        this.weight = weight;
     }
 }
