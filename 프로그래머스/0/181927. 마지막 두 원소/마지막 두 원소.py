adda0a4fd6ac17e25539a361038269991dd1fff3
ga0nii <123241025+kgw0124@users.noreply.github.com>
2024-09-03 22:34:34 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.py
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.py
@@ -1,6 +1,6 @@
 def solution(num_list):
-    value1 = num_list[len(num_list)-1] # 마지막 원소
-    value2 = num_list[len(num_list)-2] # 마지막 원소 그전 원소
+    value1 = num_list[-1] # 마지막 원소
+    value2 = num_list[-2] # 마지막 원소 그전 원소
     
     if value1 > value2:
         num_list.append(value1 - value2)
