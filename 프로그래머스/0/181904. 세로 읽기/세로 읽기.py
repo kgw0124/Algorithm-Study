fbedac6bde75cf09c2617efcfd1ecff6048d5f07
ga0nii <123241025+kgw0124@users.noreply.github.com>
2024-09-10 15:51:55 +0900

    [level 0] Title: 세로 읽기, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/0/181904. 세로 읽기/세로 읽기.py
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.py
@@ -1,8 +1,2 @@
 def solution(my_string, m, c):
-    i = 1
-    answer = ''
-    while c+(i-1)*m-1 < len(my_string):
-        answer += my_string[c+(i-1)*m-1]
-        i+=1
-
-    return answer
+        return my_string[c-1::m]
