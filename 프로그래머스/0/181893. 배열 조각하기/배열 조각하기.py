7538e15723af5b5ad74b360a3a78c80783da8baa
ga0nii <123241025+kgw0124@users.noreply.github.com>
2024-09-11 20:47:16 +0900

    [level 0] Title: 배열 조각하기, Time: 1.29 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.py
+++ 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.py
@@ -1,10 +1,7 @@
 def solution(arr, query):
-    answer = []
     for i in range(len(query)):
-        if i == 0:
-            answer = arr[:query[i]+1]
         if i%2 == 0:
-            answer = answer[:query[i]+1]
+            arr = arr[:query[i]+1]
         else:
-            answer = answer[query[i]:]
-    return answer
+            arr = arr[query[i]:]
+    return arr
