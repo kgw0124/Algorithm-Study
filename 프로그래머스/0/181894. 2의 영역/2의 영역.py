73471e9482f747f38a2b3837337c2cd66af6069a
ga0nii <123241025+kgw0124@users.noreply.github.com>
2024-09-11 20:31:41 +0900

    [level 0] Title: 2의 영역, Time: 1.67 ms, Memory: 13.5 MB -BaekjoonHub

--- 프로그래머스/0/181894. 2의 영역/2의 영역.py
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.py
@@ -1,12 +1,4 @@
 def solution(arr):
-    s = 100000
-    e = 0
-    for i in range(len(arr)):
-        if arr[i] == 2:
-            s = min(s,i)
-            e = max(e,i)
-    
-    if s>e:
-        return [-1]
-    else:
-        return arr[s:e+1]
+    if 2 in arr:
+        return arr[arr.index(2):len(arr)-arr[::-1].index(2)]
+    return [-1]
