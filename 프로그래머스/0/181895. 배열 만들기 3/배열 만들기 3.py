99a8f714a6e8d4078da919850f7866d17dbb9b6a
ga0nii <123241025+kgw0124@users.noreply.github.com>
2024-09-11 20:20:30 +0900

    [level 0] Title: 배열 만들기 3, Time: 0.94 ms, Memory: 15.6 MB -BaekjoonHub

--- 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.py
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.py
@@ -1,6 +1,5 @@
 def solution(arr, intervals):
     answer = []
     for s,e in intervals:
-        for i in arr[s:e+1]:
-            answer.append(i)
+        answer += arr[s:e+1]
     return answer
