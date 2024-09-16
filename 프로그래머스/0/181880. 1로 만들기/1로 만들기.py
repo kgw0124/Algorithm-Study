2ac8c1d61562c2eadd14ae659a590cc1ac2b5e57
ga0nii <123241025+kgw0124@users.noreply.github.com>
2024-09-16 23:30:58 +0900

    [level 0] Title: 1로 만들기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181880. 1로 만들기/1로 만들기.py
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.py
@@ -1,14 +1,8 @@
 def solution(num_list):
     total = 0
     for i in num_list:
-        count = 0
         while i != 1:
-            if i%2 == 0:
-                i /= 2
-                count += 1
-            else:
-                i = (i-1)/2
-                count += 1
-        total += count
+            i //= 2
+            total += 1
     return total
             
