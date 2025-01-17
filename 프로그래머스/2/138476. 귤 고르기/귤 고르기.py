456703090da7bfd9049d4a19f556b67313aa0a12
ga0nii <123241025+kgw0124@users.noreply.github.com>
2025-01-17 16:15:20 +0900

    [level 2] Title: 귤 고르기, Time: 22.59 ms, Memory: 21.9 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
@@ -1,21 +1,40 @@
+import collections
+
 def solution(k, tangerine):
-    # dictionary 생성
-    td = {}
-    for t in tangerine:
-        if t in td.keys():
-            td[t] = td[t] + 1
-        else:
-            td[t] = 1
+    # (1) dictionary 생성
+    dict = collections.Counter(tangerine)
     
-    # ★★ dictionary를 value를 기준으로 내림차순 정렬 ★★
-    sorted_td = dict(sorted(td.items(), key=lambda x: x[1], reverse=True))
+    # (2) value 추출 후, 정렬
+    # ★★.sort()는 list에만 사용 가능★★
+    val = sorted(dict.values(), reverse=True)
     
-    #
+    # (3) 정답 구하기
     sum = 0
     answer = 0
-    for t in sorted_td.values():
-        sum += t
+    for v in val:
+        sum += v
         answer += 1
         if sum >= k:
             break
     return answer
+    
+#     (1) dictionary 생성
+#     td = {}
+#     for t in tangerine:
+#         if t in td.keys():
+#             td[t] = td[t] + 1
+#         else:
+#             td[t] = 1
+    
+#     ★★ dictionary를 value를 기준으로 내림차순 정렬 ★★
+#     sorted_td = dict(sorted(td.items(), key=lambda x: x[1], reverse=True))
+    
+#     정답 구하기
+#     sum = 0
+#     answer = 0
+#     for t in sorted_td.values():
+#         sum += t
+#         answer += 1
+#         if sum >= k:
+#             break
+#     return answer
