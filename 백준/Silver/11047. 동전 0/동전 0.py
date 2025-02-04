06c5dcdb39646fa48d2b020dfdbdec27e20c6103
ga0nii <123241025+kgw0124@users.noreply.github.com>
2025-02-04 16:35:18 +0900

    [Silver IV] Title: 동전 0, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -0,0 +1,13 @@
+N,K = map(int, input().split(' '))
+arr = [int(input()) for i in range(N)]
+
+coins = 0
+for i in range(N-1, -1, -1): # range(N-1, 0, -1)로 하면 첫번째 요소를 접근하지 않는다.
+    if K >= arr[i]:
+        coins += K//arr[i]
+        K %= arr[i]
+    
+    if K == 0:
+        break
+
+print(coins)
