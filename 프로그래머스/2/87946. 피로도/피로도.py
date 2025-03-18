2b805ea2c2a8e6f11c003d5f90f2907e9cf469b1
ga0nii <123241025+kgw0124@users.noreply.github.com>
2025-03-18 20:50:24 +0900

    [level 2] Title: 피로도, Time: 50.57 ms, Memory: 14.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -0,0 +1,18 @@
+from itertools import permutations
+
+def solution(k, dungeons):
+    idx = [i for i in range(len(dungeons))] # [[80,20],[50,40],[30,10]]
+    idx_perm = list(permutations(idx)) # [(0, 1, 2), (0, 2, 1), (1, 0, 2), ...]
+ 
+    arr = []
+    for p in idx_perm:
+        tmp = k
+        cnt = 0
+        for i in p:
+            if tmp >= dungeons[i][0]:
+                tmp -= dungeons[i][1]
+                cnt += 1
+        arr.append(cnt)
+        
+    print(arr)
+    return max(arr)
